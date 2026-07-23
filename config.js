a96b319d2116c5473f8dfa3961e43f66aea221c6
TST64 <tstockhinger@gmail.com>
2026-07-23 11:21:25 +0200

    Auslagerung der API_URL in config.js und HTML angepasst

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // config.js
 const CONFIG = {
-    // Für Live-Betrieb (exec) hier die URL eintragen:
     API_URL: "https://script.google.com/macros/s/AKfycbyekHOsFd_g0VlShuzfH1ObfbJzYGY7DaBnBvs80deWwGKQrq_uOarL43KvmkAzcxEEcQ/exec"
   };
