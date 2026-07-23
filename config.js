06e07aae97c18d32c0ecced939ca24c1bbebb069
TST64 <tstockhinger@gmail.com>
2026-07-23 15:57:06 +0200

    Fix Spieler HCP update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbyekHOsFd_g0VlShuzfH1ObfbJzYGY7DaBnBvs80deWwGKQrq_uOarL43KvmkAzcxEEcQ/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbwQ1oqSpj8kv9PuSZ0VXpm3crQfP8BFXaWFovX1lIJEwrKYV7mSaesSNX_x1w2-VI-uQQ/exec"
   };
