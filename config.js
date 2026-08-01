251647d675cfe294faca83776b075f78a7939f3e
TST64 <tstockhinger@gmail.com>
2026-08-01 13:17:33 +0200

    Vault Sync. Version: 1.10.30  - 10.30 ist die GAS Version - BUG FIXES

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbxDeUZBNXDXTHhkWfCjEgDuq2KjmQoew9lbBIroO6EJc2ZNRGffgbdh136mVOOZDHHB-w/exec",
-    appVersion: "1.10.2"
+    API_URL: "https://script.google.com/macros/s/AKfycby5NS2VNfFj5Ec39DEtpNtn37IB92IREMGCOYlCZy5hdnRdix4jrtJygLjgbyZErtQ4tg/exec",
+    appVersion: "1.10.30"
   };
